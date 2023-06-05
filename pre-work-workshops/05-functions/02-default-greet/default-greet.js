77604de531e5ca4f2b42d889ec6a3a4a6ede97dc
aa-r-ju <aarju3447@gmail.com>
2023-06-05 14:01:56 +0545

    added default-greet

--- pre-work-workshops/05-functions/02-default-greet/default-greet.js
+++ pre-work-workshops/05-functions/02-default-greet/default-greet.js
@@ -1,1 +1,6 @@
-// YOUR CODE BELOW
+function defaultGreet(fName, lName = "Doe") {
+  console.log("Hi " + fName + " " + lName);
+  return "Hi " + fName + " " + lName + "" + "!";
+}
+defaultGreet("Aarju", "Pariyar");
+defaultGreet("John");
