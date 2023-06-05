ae0208622a1e6b2ce13bba458a20d1377a180644
aa-r-ju <aarju3447@gmail.com>
2023-06-05 17:06:27 +0545

    added my-mnemonic

--- pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
+++ pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
@@ -1,1 +1,22 @@
-// YOUR CODE BELOW
+function myMnemonic(a, b, c, d) {
+  let name = "";
+  // name = name + a[0] + b[0] + c[0] + d[0];
+  //return name;
+  if (a !== undefined) {
+    name = name + a[0];
+  }
+  if (b !== undefined) {
+    name = name + b[0];
+  }
+  if (c !== undefined) {
+    name = name + c[0];
+  }
+  if (d !== undefined) {
+    name = name + d[0];
+  }
+  console.log(myMnemonic);
+  return name;
+}
+
+console.log(myMnemonic("Must", "Accept", "Sugar", "Happiness")); // => MASH
+console.log(myMnemonic("We", "Eat", "Eggs"));
