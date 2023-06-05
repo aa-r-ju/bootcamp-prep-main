25cf94e804f8743257ffeab99a1edcae5d54da0b
aa-r-ju <aarju3447@gmail.com>
2023-06-05 22:15:12 +0545

    added do-you-play-the-theremin

--- pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
+++ pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
@@ -1,1 +1,9 @@
-// YOUR CODE BELOW
+function doYouPlayTheTheremin(name) {
+  if (name[0] === "S" || name[0] === "s") {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(doYouPlayTheTheremin("Scott"));
+console.log(doYouPlayTheTheremin("Griffin"));
