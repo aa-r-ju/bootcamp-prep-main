0d16b20d4a85021bcdf7a6146e3bf35b6099dcc6
aa-r-ju <aarju3447@gmail.com>
2023-06-05 15:00:37 +0545

    added tax-calculator

--- pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
+++ pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
@@ -1,1 +1,14 @@
-// YOUR CODE BELOW
+function taxCalculator(price, state) {
+  if (state === "NY") {
+    return (4 / 100) * price + price;
+  } else {
+    return (6.625 / 100) * price + price;
+  }
+
+  //console.log(4 / price + 100, 6.625 / price + 100);
+  //  return 4 / price + 100, 6.625 / price + 100;
+  //console.log(6.625/price + 100);
+  // return 6.625 / 100 + 100;
+}
+console.log(taxCalculator(50, "NY"));
+console.log(taxCalculator(50, "NJ"));
