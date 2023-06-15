8c7abcdab16a14d86bcf446f21ee83381fb26c7c
aa-r-ju <aarju3447@gmail.com>
2023-06-15 20:48:12 +0545

    added make-grid.js

--- workshops/06-arrays-ii/04-make-grid/make-grid.js
+++ workshops/06-arrays-ii/04-make-grid/make-grid.js
@@ -1,1 +1,43 @@
 // YOUR CODE BELOW
+// YOUR CODE BELOW
+// function makeGrid(numColumns, numRows) {
+//   let grid = [];
+//   for (let row = 0; row < numRows; row++) {
+//     let rowArray = [];
+//     for (let = col; col < numRows; col++) {
+//       rowArray.push(0);
+//     }
+//     grid.push(rowArray);
+//   }
+
+//   return grid;
+// }
+// console.log(makeGrid(3, 4));
+
+// function makeGrid(numColumns, numRows) {
+//   let grid = [];
+//   for (let row = 0; row < numRows; row++) {
+//     let rowArray = [];
+//     for (let col = 0; col < numColumns; col++) {
+//       rowArray.push(0);
+//     }
+//     grid.push(rowArray);
+//   }
+//   return grid;
+// }
+// let returnedValue = makeGrid(2, 2);
+// //let expectedValue = [
+// //[3, 4],
+// //[3, 4],
+// //];
+
+// console.log(returnedValue);
+let cat = {
+    name: "Pusheen",
+    age: 7,
+    color: "gray and tabby",
+  };
+  let keyToCheck = "name";
+  console.log(cat.name);
+  console.log();
+  
