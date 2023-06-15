002fb8fec400048b8b92ffbd7a90f1096c8b0555
aa-r-ju <aarju3447@gmail.com>
2023-06-15 20:55:58 +0545

    added frequency-analysis.js

--- workshops/07-objects/05-frequency-analysis/frequency-analysis.js
+++ workshops/07-objects/05-frequency-analysis/frequency-analysis.js
@@ -1,1 +1,35 @@
 // YOUR CODE BELOW
+// YOUR CODE BELOW
+function frequencyAnalysis(str) {
+    let frequency = {};
+    for (let i = 0; i < str.length; i++) {
+      let letter = str[i];
+      //if (str === letter ) {
+      if (frequency[letter]) {
+        frequency[letter] += 1;
+      } else {
+        frequency[letter] = 1;
+      }
+    }
+    return frequency;
+  }
+  
+  console.log(frequencyAnalysis("abcabbb"));
+  
+  // function frequencyAnalysis(str) {
+  //   let frequency = {};
+  
+  //   for (let i = 0; i < str.length; i++) {
+  //     let letter = str[i];
+  
+  //     if (frequency[letter]) {
+  //       frequency[letter] += 1;
+  //     } else {
+  //       frequency[letter] = 1;
+  //     }
+  //   }
+  
+  //   return frequency;
+  // }
+  // console.log(frequencyAnalysis("abca"));
+  
