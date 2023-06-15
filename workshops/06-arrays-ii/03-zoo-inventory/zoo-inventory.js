de4ee3bda4a2358a238dfafae0ab6df5b8493955
aa-r-ju <aarju3447@gmail.com>
2023-06-15 20:46:51 +0545

    added zoo-inventory

--- workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
+++ workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+// YOUR CODE BELOW
+function zooInventory(array) {
+    let inventory = [];
+    for (let i = 0; i < array.length; i++) {
+      let stri = " ";
+      stri = array[i][1].join(" is ");
+      array[i][1] = stri;
+      stri = array[i].join(" the ");
+      stri = stri + ".";
+      inventory.push(stri);
+    }
+  
+    return inventory;
+  }
+  //let name = [["Nemo", ["Fish", 5]]];
+  let zoo = [["Bai Yun", ["panda", 26]]];
+  
+  console.log(zooInventory(zoo));
+  
