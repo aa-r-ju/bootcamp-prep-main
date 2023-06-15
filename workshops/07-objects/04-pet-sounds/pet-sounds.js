a6e6859c208622fe8f8d99b40afd1d2c5ddd647c
aa-r-ju <aarju3447@gmail.com>
2023-06-15 20:54:35 +0545

    make pet-sounds.js

--- workshops/07-objects/04-pet-sounds/pet-sounds.js
+++ workshops/07-objects/04-pet-sounds/pet-sounds.js
@@ -36,5 +36,68 @@ let animalNoises = [
     }
   }
 ];
+let animalNoises = [
+  {
+    dog: {
+      America: "Woof! Woof!",
+      Germany: "Wau Wau!",
+      England: "Bow wow!",
+      Uruguay: "Jua jua!",
+      Afrikaans: "Blaf!",
+      Korea: "Mong mong!",
+      Iceland: "Voff voff!",
+      Albania: "Ham!",
+      Algeria: "Ouaf ouaf!",
+    },
+  },
+  {
+    cat: {
+      America: "Meow",
+      Germany: "Miauw!",
+      England: "mew mew",
+      Uruguay: "Miau Miau!",
+      Afrikaans: "Purr",
+      Korea: "Nyaong!",
+      Iceland: "Kurnau!",
+      Albania: "Miau",
+      Algeria: "Miaou!",
+    },
+  },
+  {
+    chicken: {
+      America: "Cluck cluck",
+      Germany: "tock tock tock",
+      England: "Cluck Cluck",
+      Uruguay: "gut gut gdak",
+      Afrikaans: "kukeleku",
+      Korea: "ko-ko-ko",
+      Iceland: "Chuck-chuck!",
+      Albania: "Kotkot",
+      Algeria: "Cotcotcodet",
+    },
+  },
+];
+
+// YOUR CODE BELOW
+function petSounds(animal, country) {
+  let finalString = "";
+  for (let l = 0; l < animalNoises.length; l++) {
+    for (key in animalNoises[l]) {
+      if (animal === key) {
+        console.log(animalNoises[l][key][country]);
+        finalString =
+          animal.charAt(0).toUpperCase() +
+          animal.slice(1) +
+          "s " +
+          "in " +
+          country +
+          " say " +
+          animalNoises[l][key][country];
+      }
+    }
+  }
+  return finalString;
+}
+console.log(petSounds("dog", "Germany"));
 
 // YOUR CODE BELOW
