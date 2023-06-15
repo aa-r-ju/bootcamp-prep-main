9e879a5027e45f4356305b3f27815596e84cc99a
aa-r-ju <aarju3447@gmail.com>
2023-06-15 13:46:46 +0545

    new commit

--- workshops/02-loops/01-only-odds/only-odds.js
+++ workshops/02-loops/01-only-odds/only-odds.js
@@ -1,1 +1,12 @@
-// YOUR CODE BELOW
+function onlyOdds(num) {
+  if (num < 1);
+  return 0;
+}
+let sum = 0;
+for (let i = num; i >= 1; i--) {
+  if (i % 2 !== 0) {
+    sum += i;
+  }
+  return sum;
+}
+console.log(onlyOdds(6));
