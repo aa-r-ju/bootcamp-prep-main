344b1ab2c80091a7a21da5b8e54adf3638ba77f5
aa-r-ju <aarju3447@gmail.com>
2023-06-15 20:45:00 +0545

    added array-flattener.js

--- workshops/06-arrays-ii/02-array-flattener/array-flattener.js
+++ workshops/06-arrays-ii/02-array-flattener/array-flattener.js
@@ -1,1 +1,39 @@
 // YOUR CODE BELOW
+// YOUR CODE BELOW
+// function arrayFlattener(array) {
+//   let arrayFlattener = [];
+
+//   function flatten(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//       if (Array.isArray(arr[i])) {
+//         flatten(arr[i]);
+//       } else {
+//         flattenedArray.push(arr[i]);
+//       }
+//     }
+//   }
+//   flatten(array);
+//   return flattenedArray;
+// }
+
+//console.log(arrayFlattener([1, [2, 3], 4]));
+
+// arrayFlattener([1,[2, 3], 4])
+
+function arrayFlattener(array) {
+    let flattenedArray = [];
+  
+    function flatten(arr) {
+      for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+          flatten(arr[i]);
+        } else {
+          flattenedArray.push(arr[i]);
+        }
+      }
+    }
+  
+    flatten(array);
+    return flattenedArray;
+  }
+  
