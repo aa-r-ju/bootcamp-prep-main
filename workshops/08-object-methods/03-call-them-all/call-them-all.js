1d8d6cfc6b2f003690b1ccfafa06b609665ade2e
aa-r-ju <aarju3447@gmail.com>
2023-06-15 21:12:07 +0545

    added tic-tac-toe.js

--- workshops/08-object-methods/03-call-them-all/call-them-all.js
+++ workshops/08-object-methods/03-call-them-all/call-them-all.js
@@ -1,1 +1,27 @@
-// YOUR CODE BELOW
+// YOUR CODE BELOW// YOUR CODE BELOW
+function callThemAll(object, value) {
+    debugger;
+    let val = [];
+    for (let key in object) {
+      //console.log(key);
+      // console.log(object[key]);
+      //console.log(typeof object[key]);
+      if (typeof object[key] === "function") val.push(object[key](value));
+    }
+    return val;
+  }
+  
+  // let obj = {
+  //   addTen: function (num) {
+  //     return num + 10;
+  //   },
+  
+  //   addTwenty: function (num) {
+  //     return num + 20;
+  //   },
+  
+  //   someProperty: "value",
+  // };
+  
+  console.log(callThemAll(obj, 100)); // => [110, 120]
+  
--- workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
+++ workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
@@ -1,2 +1,24 @@
-console.log("hello");
-hello;
+et ticTacToe = {
+    board: [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ],
+    move: function (character, rowNum, colNum) {
+      this.board[rowNum][colNum] = character;
+      console.log(this.board);
+      if (ticTacToe.board[1][1] === character) {
+        console.log("Test passed!");
+      } else {
+        console.log("Test failed!");
+      }
+  
+      return this.board;
+    },
+    clear: function () {},
+  };
+  
+  for (let i = 0; i < 5; i++) {
+    console.log("Hello world");
+  }
+  
