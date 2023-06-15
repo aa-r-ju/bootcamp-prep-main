f33ff567ee95c4fe9a89d49983f7f7ced5e8d6e5
aa-r-ju <aarju3447@gmail.com>
2023-06-15 20:43:53 +0545

    make even-and-odd.js

--- workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
+++ workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+// YOUR CODE BELOW
+function evenAndOdd(array) {
+    debugger;
+    let even = [];
+    let odd = [];
+  
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] % 2 === 0) {
+        even.push(array[i]);
+      } else {
+        odd.push(array[i]);
+      }
+    }
+  
+    return [even, odd];
+  }
+  
+  console.log(evenAndOdd([1, 2, 3, 4, 5, 6]));
+  
