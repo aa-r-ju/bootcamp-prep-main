8fdb526fbacfe6590b67e10c590cecde3731d594
aa-r-ju <aarju3447@gmail.com>
2023-06-15 20:50:35 +0545

    make code for last-friday-night.js

--- workshops/07-objects/01-last-friday-night/last-friday-night.js
+++ workshops/07-objects/01-last-friday-night/last-friday-night.js
@@ -1,1 +1,32 @@
 // YOUR CODE BELOW
+function lastFridayNight(transactions) {
+    let totalAmount = 0;
+  
+    for (let i = 0; i < transactions.length; i++) {
+      let transaction = transactions[i];
+      totalAmount += transaction.amount;
+    }
+  
+    return totalAmount;
+  }
+  let transactions = [
+    {
+      name: "Tons of glitter",
+      amount: 70,
+    },
+    {
+      name: "Porcelain Pink Flamingos",
+      amount: 92,
+    },
+    {
+      name: "Chandelier replacement",
+      amount: 10000,
+    },
+    {
+      name: "Dinner at TGIF x6",
+      amount: 350,
+    },
+  ];
+  
+  console.log(lastFridayNight(transactions)); // Output: 10512
+  
