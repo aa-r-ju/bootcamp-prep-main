cc6e0e608e41dc10c4bc7b5d9e0be88a669e53c2
aa-r-ju <aarju3447@gmail.com>
2023-06-15 20:21:46 +0545

    added my-last-index.js

--- workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
+++ workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
@@ -1,1 +1,22 @@
 // YOUR CODE BELOW
+// YOUR CODE BELOW
+function myLastIndexOf(array, searchValue, startIdx) {
+    if (startIdx === undefined) {
+      startIdx = array.length - 1;
+    }
+  
+    for (let i = startIdx; i >= 0; i--) {
+      if (array[i] === searchValue) {
+        return i;
+      }
+    }
+  
+    return -1;
+  }
+  
+  console.log(myLastIndexOf(["gee", "gee", "gee", "gee", "baby", "baby"], "gee"));
+  console.log(myLastIndexOf(["Tiffany", "Sunny", "Yoona"], "Jessica"));
+  console.log(
+    myLastIndexOf(["the", "girls", "bring", "the", "boys", "out"], "the", 2)
+  );
+  
