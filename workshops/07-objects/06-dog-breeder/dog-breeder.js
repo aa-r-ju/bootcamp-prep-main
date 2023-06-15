ef69c51c42c592a8af0688520a684189e6d80c81
aa-r-ju <aarju3447@gmail.com>
2023-06-15 20:57:12 +0545

    make dog-breeder.js

--- workshops/07-objects/06-dog-breeder/dog-breeder.js
+++ workshops/07-objects/06-dog-breeder/dog-breeder.js
@@ -1,1 +1,37 @@
 // YOUR CODE BELOW
+// function dogBreeder(name, age = 0) {
+//   debugger;
+//   let dog = {};
+
+//   if (typeof name === "string") {
+//     dog.name = name;
+//   } else {
+//     dog.name = "Steve";
+//   }
+//   if (typeof age === "number") {
+//     dog.age = age;
+//   } else {
+//     dog.age = 0;
+//   }
+//   return dog;
+// }
+// console.log(dogBreeder("Snoopy"));
+// console.log(dogBreeder(3));
+
+function dogBreeder(name, age = 0) {
+    debugger;
+    let dog = {};
+    if (typeof name === "number") {
+      dog.name = "Steve";
+      dog.age = name;
+    } else {
+      dog.name = name || "Steve";
+  
+      dog.age = age || 0;
+    }
+    return dog;
+  }
+  
+  //console.log(dogBreeder("Snoopy"));
+  console.log(dogBreeder(3));
+  
