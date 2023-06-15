7c227fd13def0bd5e752c3852490a6dcf68d3701
aa-r-ju <aarju3447@gmail.com>
2023-06-15 20:23:12 +0545

    make my-reverse.js

--- workshops/05-arrays-i/04-my-reverse/my-reverse.js
+++ workshops/05-arrays-i/04-my-reverse/my-reverse.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+// YOUR CODE BELOW
+function myReverse(arr) {
+    let reversedArray = [];
+  
+    for (let i = arr.length - 1; i >= 0; i--) {
+      reversedArray.push(arr[i]);
+    }
+  
+    return reversedArray;
+  }
+  console.log(myReverse([1, 2, 3])); // Output: [3, 2, 1]
+  
