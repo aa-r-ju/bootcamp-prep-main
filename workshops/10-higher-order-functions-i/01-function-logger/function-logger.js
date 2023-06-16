d2380d83ca6947f89547561c5ba01af3d5a97cf8
aa-r-ju <aarju3447@gmail.com>
2023-06-16 14:57:23 +0545

    added function-logger

--- workshops/10-higher-order-functions-i/01-function-logger/function-logger.js
+++ workshops/10-higher-order-functions-i/01-function-logger/function-logger.js
@@ -1,1 +1,13 @@
-// YOUR CODE BELOW
+function functionLogger(func, arg) {
+    console.log("Function starting");
+    let  result = func(arg);
+    console.log("Function complete");
+    return result;
+  }
+  
+  function squareNum(num) {
+    return num * num;
+  }
+  
+  let squareOfFour = functionLogger(squareNum, 4);
+  console.log(squareOfFour); // Output: 16
--- workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
+++ workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
@@ -1,1 +1,20 @@
-// YOUR CODE BELOW
+// YOUR CODE BELOWf
+function finderFunction(array,myFunc ) {
+    for(let i = 0 ; i < array.length; i++) {
+        if(myFunc(array[i])){
+            return i;
+        }
+    }
+     return -1;
+
+
+}
+let numbers = [1, 3, 5, 64, 7, 12];
+let odds = [9, 13, 15, 17];
+
+function isEven(num) {
+  return !(num % 2);
+}
+
+console.log(finderFunction(numbers, isEven)) // 3
+console.log(finderFunction(odds, isEven)) // -1
