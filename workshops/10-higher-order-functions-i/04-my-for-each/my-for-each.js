9afe6d6c07c1fd1f09a162897af20e8d46b428c2
aa-r-ju <aarju3447@gmail.com>
2023-06-17 10:31:44 +0545

    added chain-reaction.js

--- workshops/10-higher-order-functions-i/04-my-for-each/my-for-each.js
+++ workshops/10-higher-order-functions-i/04-my-for-each/my-for-each.js
@@ -1,1 +1,21 @@
-// YOUR CODE BELOW
+function myForEach(array, callback) {
+    for (let i = 0; i < array.length; i++) {
+      callback(array[i], i, array);
+    }
+    let pop = ""
+    return pop
+  }
+   
+  let sum = 0;
+  
+  function addToSum(num) {
+    sum += num;
+    
+  }
+  
+  let nums = [1, 2, 3];
+  
+  myForEach(nums, addToSum);
+  
+  console.log(sum); // Output: 6
+  
--- workshops/10-higher-order-functions-i/05-chain-reaction/chain-reaction.js
+++ workshops/10-higher-order-functions-i/05-chain-reaction/chain-reaction.js
@@ -1,1 +1,26 @@
 // YOUR CODE BELOW
+function chainReaction(startingValue, functions) {
+    let result = startingValue;
+    
+    for (let i = 0; i < functions.length; i++) {
+      result = functions[i](result);
+    }
+    
+    return result;
+  }
+  
+
+  function addTen(num) {
+    return num + 10;
+  }
+  
+  function subtractFive(num) {
+    return num - 5;
+  }
+  
+  function multiplyFive(num) {
+    return num * 5;
+  }
+  
+  console.log(chainReaction(0, [addTen, subtractFive, multiplyFive])); 
+  console.log(chainReaction(0, [subtractFive, multiplyFive, addTen])); 
