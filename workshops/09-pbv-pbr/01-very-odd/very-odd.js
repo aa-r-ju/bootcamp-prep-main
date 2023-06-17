5ca8c8bf0f37b649927ac6bfb988352681d79151
aa-r-ju <aarju3447@gmail.com>
2023-06-17 14:37:33 +0545

    commited very-odd.js

--- workshops/09-pbv-pbr/01-very-odd/very-odd.js
+++ workshops/09-pbv-pbr/01-very-odd/very-odd.js
@@ -1,1 +1,11 @@
-// YOUR CODE BELOW
+function veryOdd(numbers) {
+    const oddNumbers = numbers.filter(num => num % 2 !== 0);
+    return oddNumbers;
+  }
+  
+  let allTheNums = [1, 2, 3, 4, 5, 6, 7, 8];
+  let oddNums = veryOdd(allTheNums);
+  
+  console.log('oddNums:', oddNums); 
+  console.log('allTheNums:', allTheNums); 
+  
