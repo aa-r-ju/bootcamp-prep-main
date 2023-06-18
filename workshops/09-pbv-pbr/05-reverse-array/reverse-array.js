701cd35cde2abf6bcbd028336d0f74bc1dd15703
aa-r-ju <aarju3447@gmail.com>
2023-06-18 11:42:57 +0545

    added deeper-copy.js

--- workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
+++ workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
@@ -1,1 +1,21 @@
 // YOUR CODE BELOW
+function reverse(array) {
+    var leftIndex = 0;
+    var rightIndex = array.length - 1;
+  
+    while (leftIndex < rightIndex) {
+      // Swap elements at leftIndex and rightIndex
+      var temp = array[leftIndex];
+      array[leftIndex] = array[rightIndex];
+      array[rightIndex] = temp;
+  
+      // Move the indices towards the center
+      leftIndex++;
+      rightIndex--;
+    }
+  }
+  
+  var myArray = [1, 2, 3, 4];
+  reverse(myArray);
+  console.log(myArray); // [4, 3, 2, 1]
+  
--- workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
+++ workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
@@ -1,1 +1,26 @@
 // YOUR CODE BELOW
+function deeperCopy(array) {
+    var newArray = [];
+  
+    for (var i = 0; i < array.length; i++) {
+      if (Array.isArray(array[i])) {
+        newArray[i] = deeperCopy(array[i]);
+      } else {
+        newArray[i] = array[i];
+      }
+    }
+  
+    return newArray;
+  }
+  
+  var myArray = [1, [2, 3]];
+  var copy = deeperCopy(myArray);
+  copy[1].push(4);
+  
+  console.log(myArray);
+  
+  var copy2 = myArray.slice();
+  copy2[1].push(4);
+  
+  console.log(myArray);
+  
