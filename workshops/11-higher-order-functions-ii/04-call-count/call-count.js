be0240bb79c514f855a3248b395841216f7c9cf8
aa-r-ju <aarju3447@gmail.com>
2023-06-19 12:47:12 +0545

    commited call-count.js

--- workshops/11-higher-order-functions-ii/04-call-count/call-count.js
+++ workshops/11-higher-order-functions-ii/04-call-count/call-count.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+function callCount() {
+    let count = 0;
+  
+    return function() {
+      count++;
+      return count;
+    };
+  }
+  
+  let newFunction1 = callCount();
+  console.log(newFunction1()); // Output: 1
+  console.log(newFunction1()); // Output: 2
+  
+  let newFunction2 = callCount();
+  console.log(newFunction2()); // Output: 1
+  console.log(newFunction2()); // Output: 2
+  Explanation
