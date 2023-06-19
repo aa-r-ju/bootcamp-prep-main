ea64ba8c048ec6211903caa63a63bc5c0695554e
aa-r-ju <aarju3447@gmail.com>
2023-06-19 12:49:58 +0545

    commited stringify.js

--- workshops/11-higher-order-functions-ii/05-stringify/stringify.js
+++ workshops/11-higher-order-functions-ii/05-stringify/stringify.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function stringify(callback) {
+    return function() {
+      let result = callback();
+      return String(result);
+    };
+  }
+  
+  function returnsANumber() {
+    return 100;
+  }
+  
+  let newFunction = stringify(returnsANumber);
+  console.log(newFunction()); // Output: '100'
