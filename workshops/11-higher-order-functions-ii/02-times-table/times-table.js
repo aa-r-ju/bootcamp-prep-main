f62a954a3dd93e032aa59ddecd57871ce765b1e8
aa-r-ju <aarju3447@gmail.com>
2023-06-19 12:43:52 +0545

    commited times-table.js

--- workshops/11-higher-order-functions-ii/02-times-table/times-table.js
+++ workshops/11-higher-order-functions-ii/02-times-table/times-table.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function timesTable(number) {
+    return function(multiplier) {
+      return number * multiplier;
+    };
+  }
+  
+  let ninesTable = timesTable(9);
+  console.log(ninesTable(8)); // Output: 72
+  
+  let twelvesTable = timesTable(12);
+  console.log(twelvesTable(100)); // Output: 1200
