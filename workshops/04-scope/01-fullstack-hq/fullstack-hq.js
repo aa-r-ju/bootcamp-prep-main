d6b4f62bd864d6bb20fedbc6772c3090cd91375c
aa-r-ju <aarju3447@gmail.com>
2023-07-10 14:14:23 +0545

    fix:parameter changes

--- workshops/04-scope/01-fullstack-hq/fullstack-hq.js
+++ workshops/04-scope/01-fullstack-hq/fullstack-hq.js
@@ -19,15 +19,43 @@
 // }
 
 
-function fullstackHQ() {
-  var planet = "Planet Earth";
-  var country = "United States";
-  var state = "New York State";
-  var city = "New York City";
+// function fullstackHQ() {
+//   var planet = "Planet Earth";
+//   var country = "United States";
+//   var state = "New York State";
+//   var city = "New York City";
 
-  return "Fullstack HQ is at " + planet + ", " + country + ", " + state + ", " + city;
+//   return "Fullstack HQ is at " + planet + ", " + country + ", " + state + ", " + city;
+// }
+
+// console.log(fullstackHQ());
+// // Output: Fullstack HQ is at Planet Earth, United States, New York State, New York City
+
+
+
+
+
+
+
+let earth = 'Planet Earth';
+
+function fullstackHQ(){
+  return middleFunction('United States');
+
+  function middleFunction(state) {
+    return innerFunction('New York State');
+
+    function innerFunction(city) {
+     place = 'New York City';
+      return innermostFunction();
+
+      function innermostFunction() {
+
+        return 'Fullstack HQ is at ' + earth + ', ' + state + ', ' + city + ', ' + place;
+      }
+    }
+  }
 }
-
-console.log(fullstackHQ());
-// Output: Fullstack HQ is at Planet Earth, United States, New York State, New York City
+debugger
+console.log(fullstackHQ())
 
