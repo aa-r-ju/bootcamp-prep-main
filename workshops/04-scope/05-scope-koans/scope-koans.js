e33d374715d0a04cd3ed3ecd537c498b63c279b9
aa-r-ju <aarju3447@gmail.com>
2023-07-10 16:01:51 +0545

    fix:bugs

--- workshops/04-scope/05-scope-koans/scope-koans.js
+++ workshops/04-scope/05-scope-koans/scope-koans.js
@@ -1,76 +1,14 @@
 // Test One Restrictions: Do not declare any new variable with the let keyword
-// let testOneMessage = 'test failing';
-
-// function testOne() {
-//   return testOneMessage;
-// }
-
-
-// // Test Two Restrictions: Do not change any code in the body of testTwo
-// // (you can edit helperFunc though)
-// let testTwoMessage = 'test failing';
-
-// function testTwo() {
-//   helperFunc();
-//   return testTwoMessage;
-// }
-
-// function helperFunc(a) {
-//   a = 'test succeeding';
-//   return a;
-// }
-
-
-// // Test Three Restrictions: Type only a single character in either testThree or getMessage
-// let testThreeMessage = 'test failing';
-
-// function testThree(testThreeMessage) {
-//   if (testThreeMessage) {
-//     testThreeMessage = 'test succeeding';
-//   }
-
-//   let msg = getMessage();
-//   return msg;
-
-//   function getMessage(value) {
-//     if (value) {
-//       return testThreeMessage;
-//     }
-
-//     return 'test failing';
-//   }
-// }
-
-
-// // Test Four Restrictions: Delete only a single character from any function.
-// let testFourMessage = 'test succeeding';
-
-// function testFour(msg) {
-//   function innerFunc(msg) {
-//     msg = msg
-
-//     function doubleInner(msg) {
-//       testFourMessage = msg;
-//       return testFourMessage;
-//     }
-
-//     testFourMessage = doubleInner('test failing')
-//   }
-
-//   innerFunc(testFourMessage);
-
-//   msg = testFourMessage;
-//   return testFourMessage;
-// }
-
-
 let testOneMessage = 'test failing';
+testOneMessage = "test succeeding"
 
 function testOne() {
-  testOneMessage = "test succeeding"
   return testOneMessage;
 }
 
+
+// Test Two Restrictions: Do not change any code in the body of testTwo
+// (you can edit helperFunc though)
 let testTwoMessage = 'test failing';
 
 function testTwo() {
@@ -78,22 +16,23 @@ function testTwo() {
   return testTwoMessage;
 }
 
-function helperFunc() {
-  testTwoMessage = 'test succeeding';
+function helperFunc(a) {
+ testTwoMessage = 'test succeeding';
+  return testTwoMessage;
 }
-
+// Test Three Restrictions: Type only a single character in either testThree or getMessage
 let testThreeMessage = 'test failing';
 
-function testThree() {
+function testThree(testThreeMessage) {
   if (testThreeMessage) {
     testThreeMessage = 'test succeeding';
   }
 
-  let msg = getMessage();
+  let msg = getMessage('a'); //added "a"
   return msg;
 
-  function getMessage() {
-    if (testThreeMessage) {
+  function getMessage(value) {
+    if (value) {
       return testThreeMessage;
     }
 
@@ -101,22 +40,29 @@ function testThree() {
   }
 }
 
+
+// Test Four Restrictions: Delete only a single character from any function.
 let testFourMessage = 'test succeeding';
 
-function testFour(msg) {
-  function innerFunc() {
-    let msg = testFourMessage;
+function testFour(msg) 
+{
+  function innerFunc(msg) {
+    msg = msg
 
-    function doubleInner() {
-     
+    function doubleInner(mg) {
+      testFourMessage = msg;
       return testFourMessage;
     }
 
-    testFourMessage = doubleInner(); 
+    testFourMessage = doubleInner('test failing')
   }
 
-  innerFunc();
+  innerFunc(testFourMessage);
 
   msg = testFourMessage;
   return testFourMessage;
 }
+
+
+
+
