11d7b015f092a259c4be1c54cccbc06590d234de
aa-r-ju <aarju3447@gmail.com>
2026-07-29 20:25:43 +0545

    feat: bacteria-time

--- workshops/02-loops/03-bacteria-time/bacteria-time.js
+++ workshops/02-loops/03-bacteria-time/bacteria-time.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function bacteriaTime(currentNum, targetNum) {
+  let time = 0;
+
+  if (targetNum < currentNum) {
+    return "targetNum must be larger than currentNum";
+  }
+  for (let i = currentNum; i < targetNum; i *= 2) {
+    time += 20;
+  }
+
+  return time;
+}
+console.log(bacteriaTime(1, 8));
+console.log(bacteriaTime(8, 5));
