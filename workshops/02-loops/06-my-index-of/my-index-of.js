9a922ac8c822d9ece8ccff33fa72ed955e328eae
aa-r-ju <aarju3447@gmail.com>
2026-08-01 23:32:19 +0545

    feat: my-index-of

--- workshops/02-loops/06-my-index-of/my-index-of.js
+++ workshops/02-loops/06-my-index-of/my-index-of.js
@@ -1,1 +1,29 @@
 // YOUR CODE BELOW
+function myIndexOf(source, searchValue, startIdx = 0) {
+  for (let i = startIdx; i <= source.length - searchValue.length; i++) {
+    let found = true;
+
+    for (let j = 0; j < searchValue.length; j++) {
+      if (source[i + j] !== searchValue[j]) {
+        found = false;
+        break;
+      }
+    }
+
+    if (found) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+console.log(myIndexOf("hello", "h"));
+console.log(myIndexOf("hello", "e"));
+console.log(myIndexOf("hello", "l"));
+console.log(myIndexOf("hello", "l"));
+console.log(myIndexOf("hello", "o"));
+
+console.log(myIndexOf("twice twice", "ice"));
+console.log(myIndexOf("twice twice", "ice", 5));
+console.log(myIndexOf("happy string", "sad"));
