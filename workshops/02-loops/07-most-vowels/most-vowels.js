5f63ee0b1bcf8fae413eb7129beaf117d6a95b9c
aa-r-ju <aarju3447@gmail.com>
2026-08-02 20:56:16 +0545

    feat: most-vowels

--- workshops/02-loops/07-most-vowels/most-vowels.js
+++ workshops/02-loops/07-most-vowels/most-vowels.js
@@ -1,1 +1,28 @@
 // YOUR CODE BELOW
+function mostVowels(str) {
+  let high = 0;
+  let result = "";
+
+  let arr = str.split(" ");
+  for (let i = 0; i < arr.length; i++) {
+    let word = arr[i].replace(/[.,!?]/g, "");
+
+    let count = 0;
+
+    for (let j = 0; j < word.length; j++) {
+      let ch = arr[i][j].toLowerCase();
+      if (ch === "a" || ch === "e" || ch === "i" || ch === "o" || ch === "u") {
+        count++;
+      }
+    }
+    if (count > high) {
+      high = count;
+      result = word;
+    }
+  }
+  return result;
+}
+
+console.log(mostVowels("try my gym"));
+console.log(mostVowels("I am a keeper with some real rhythms"));
+console.log(mostVowels("HELLO world!")); // "HELLO"
