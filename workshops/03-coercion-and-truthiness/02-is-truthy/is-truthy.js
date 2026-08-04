5b603547f402995b222e502696c0dd9fb6f29aab
aa-r-ju <aarju3447@gmail.com>
2026-08-04 18:08:27 +0545

    feat: is-truthy

--- workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
+++ workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
@@ -1,1 +1,22 @@
 // YOUR CODE BELOW
+function isTruthy(arg) {
+  if (arg === false) {
+    return "The boolean value false is falsey";
+  } else if (arg === null) {
+    return "The null value is falsey";
+  } else if (arg === undefined) {
+    return "undefined is falsey";
+  } else if (arg === 0) {
+    return "The number 0 is falsey (the only falsey number)";
+  } else if (arg === "") {
+    return "The empty string is falsey (the only falsey string)";
+  } else {
+    return true;
+  }
+}
+console.log(isTruthy("I yearn for truth"));
+console.log(isTruthy(null));
+console.log(isTruthy(false));
+console.log(isTruthy(undefined));
+console.log(isTruthy(0));
+console.log(isTruthy(""));
