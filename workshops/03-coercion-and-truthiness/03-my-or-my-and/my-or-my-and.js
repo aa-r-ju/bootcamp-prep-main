566cedab9042d507db7df46e5bf0b34e4a9d4945
aa-r-ju <aarju3447@gmail.com>
2026-08-05 17:28:39 +0545

    feat: my-or

--- workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
+++ workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function myOr(...arg) {
+  for (let i = 0; i < arg.length; i++) {
+    if (arg[i]) {
+      return arg[i];
+    }
+  }
+  return arg[arg.length - 1];
+}
+console.log(myOr(true, true, false));
+console.log(myOr(false, false, true));
+console.log(myOr(false, false, false));
+console.log(myOr("apple", false, false));
+console.log(myOr(0, "", false));
