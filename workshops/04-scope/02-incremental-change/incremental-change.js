5b2d6f0196362504984debdd948379c58a9b7313
aa-r-ju <aarju3447@gmail.com>
2026-08-06 21:58:46 +0545

    feat: increament-change

--- workshops/04-scope/02-incremental-change/incremental-change.js
+++ workshops/04-scope/02-incremental-change/incremental-change.js
@@ -1,3 +1,10 @@
 let counter = 0;
 
 // YOUR CODE BELOW
+function incrementalChange(number) {
+  counter += number;
+  return counter;
+}
+console.log(incrementalChange(2));
+console.log(incrementalChange(6));
+console.log(incrementalChange(-1));
