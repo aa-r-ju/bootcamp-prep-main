607769ae6d46da4343d4907b4dcafb5a5af1b433
aa-r-ju <aarju3447@gmail.com>
2026-08-06 21:51:09 +0545

    feat: fullstack-hq

--- workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
+++ workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+function zeroDarkThirty(number) {
+  if (number === 0) {
+    return NaN;
+  }
+  let str = String(number);
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== "0") {
+      result += str[i];
+    }
+  }
+  return Number(result);
+}
+console.log(zeroDarkThirty(102302));
+console.log(zeroDarkThirty(606.203));
+console.log(zeroDarkThirty(0));
