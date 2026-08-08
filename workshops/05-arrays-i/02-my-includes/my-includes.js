d8c54e4fb10308dc6d6328846e91f311449f29b4
aa-r-ju <aarju3447@gmail.com>
2026-08-08 12:54:01 +0545

    feat: my-includes

--- workshops/05-arrays-i/02-my-includes/my-includes.js
+++ workshops/05-arrays-i/02-my-includes/my-includes.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function myIncludes(arr, searchValue) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === searchValue) {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(myIncludes([10, 20, 30], 20));
+console.log(myIncludes(["apples", "bananas", "citrus"], "pears"));
