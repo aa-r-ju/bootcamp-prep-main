0d17be4242a874620a25d2c67a7ec5d84bb3a660
aa-r-ju <aarju3447@gmail.com>
2026-08-08 22:15:18 +0545

    feat: my-join

--- workshops/06-arrays-ii/06-my-join/my-join.js
+++ workshops/06-arrays-ii/06-my-join/my-join.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+function myJoin(array, separator = ",") {
+  let str = "";
+  for (let i = 0; i < array.length; i++) {
+    let value = array[i];
+    if (value === undefined || value === null) {
+      value = "";
+    }
+    str += value;
+
+    if (i !== array.length - 1) {
+      str += separator;
+    }
+  }
+  return str;
+}
+console.log(myJoin(["let's", "make", "a", "list"], " "));
+console.log(myJoin(["a", "b", "c"], "+"));
+console.log(myJoin(["Peter", "Paul", "Mary"]));
+console.log(myJoin(["hello", undefined, "world"], "-"));
