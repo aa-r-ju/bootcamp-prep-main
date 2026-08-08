80360f56b4648ba2adf990e2711536dfb8039dfb
aa-r-ju <aarju3447@gmail.com>
2026-08-08 14:29:45 +0545

    feat: even-and-odd

--- workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
+++ workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function evenAndOdd(arr) {
+  let odd = [];
+  let even = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) {
+      odd.push(arr[i]);
+    } else {
+      even.push(arr[i]);
+    }
+  }
+  return [even, odd];
+}
+console.log(evenAndOdd([1, 2, 3, 4, 5, 6]));
