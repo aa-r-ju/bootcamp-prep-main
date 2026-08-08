08f9abee52255c9f3a533334708b36fa79fc5f3d
aa-r-ju <aarju3447@gmail.com>
2026-08-08 15:50:11 +0545

    feat: array-flattener

--- workshops/06-arrays-ii/02-array-flattener/array-flattener.js
+++ workshops/06-arrays-ii/02-array-flattener/array-flattener.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function arrayFlattener(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let j = 0; j < arr[i].length; j++) {
+        result.push(arr[i][j]);
+      }
+    } else {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+console.log(arrayFlattener([1, [2, 3], 4]));
