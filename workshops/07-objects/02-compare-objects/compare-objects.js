c5c6ea07ad696140e84ba6f9b9869ef06fe30013
aa-r-ju <aarju3447@gmail.com>
2026-08-09 21:59:13 +0545

    feat: compare-objects

--- workshops/07-objects/02-compare-objects/compare-objects.js
+++ workshops/07-objects/02-compare-objects/compare-objects.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+function compareObjects(obj1, obj2) {
+  let first = Object.keys(obj1);
+  let second = Object.keys(obj2);
+
+  if (first.length !== second.length) {
+    return false;
+  }
+
+  for (let i = 0; i < first.length; i++) {
+    let key = first[i];
+
+    if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(compareObjects({ name: "nick" }, { name: "nick" }));
+console.log(compareObjects({ name: "zeke" }, { name: "zeke", age: 12 }));
