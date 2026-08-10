1e2a81701a8d7ad815ffbc6e6addbc2ff18e17f7
aa-r-ju <aarju3447@gmail.com>
2026-08-10 15:51:31 +0545

    feat: frequency-analysis

--- workshops/07-objects/05-frequency-analysis/frequency-analysis.js
+++ workshops/07-objects/05-frequency-analysis/frequency-analysis.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+function frequencyAnalysis(str) {
+  let obj = {};
+  for (let i = 0; i < str.length; i++) {
+    let small = str[i].toLowerCase();
+    if (small in obj) {
+      obj[small] += 1;
+    } else {
+      obj[small] = 1;
+    }
+  }
+  return obj;
+}
+console.log(frequencyAnalysis("abcd"));
+console.log(frequencyAnalysis("abca"));
+console.log(frequencyAnalysis("ababababab"));
