152845af3ccb4da9e899023c00795ca2d5cbfba1
aa-r-ju <aarju3447@gmail.com>
2026-08-10 16:25:17 +0545

    feat: dog-breeder

--- workshops/07-objects/06-dog-breeder/dog-breeder.js
+++ workshops/07-objects/06-dog-breeder/dog-breeder.js
@@ -1,1 +1,19 @@
 // YOUR CODE BELOW
+function dogBreeder(name, age) {
+  if (typeof name === "number") {
+    age = name;
+    name = "Steve";
+  }
+  if (name === undefined) {
+    name = "Steve";
+  }
+
+  if (age === undefined) {
+    age = 0;
+  }
+  return { name, age };
+}
+console.log(dogBreeder("sam", 12));
+console.log(dogBreeder(15));
+console.log(dogBreeder("kiku"));
+console.log(dogBreeder(undefined, 15));
