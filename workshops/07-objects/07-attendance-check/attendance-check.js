1ef95e7e2292d1897544f799011a864cc1af2e6d
aa-r-ju <aarju3447@gmail.com>
2026-08-11 17:02:33 +0545

    feat: attendance-check

--- workshops/07-objects/07-attendance-check/attendance-check.js
+++ workshops/07-objects/07-attendance-check/attendance-check.js
@@ -1,40 +1,56 @@
 let classRoom = [
-    {
-        "Marnie" : [
-                {"Monday" : true},
-                {"Tuesday" : true},
-                {"Wednesday" : true},
-                {"Thursday" : true},
-                {"Friday" : true}
-            ]
-    },
-    {
-        "Lena" : [
-                {"Monday" : false},
-                {"Tuesday" : false},
-                {"Wednesday" : true},
-                {"Thursday" : false},
-                {"Friday" : true}
-            ]
-    },
-    {
-        "Shoshanna" : [
-                {"Monday" : true},
-                {"Tuesday" : true},
-                {"Wednesday" : false},
-                {"Thursday" : true},
-                {"Friday" : false}
-            ]
-    },
-    {
-        "Jessa" : [
-                {"Monday" : false},
-                {"Tuesday" : false},
-                {"Wednesday" : false},
-                {"Thursday" : false},
-                {"Friday" : true}
-            ]
-    }
+  {
+    Marnie: [
+      { Monday: true },
+      { Tuesday: true },
+      { Wednesday: true },
+      { Thursday: true },
+      { Friday: true },
+    ],
+  },
+  {
+    Lena: [
+      { Monday: false },
+      { Tuesday: false },
+      { Wednesday: true },
+      { Thursday: false },
+      { Friday: true },
+    ],
+  },
+  {
+    Shoshanna: [
+      { Monday: true },
+      { Tuesday: true },
+      { Wednesday: false },
+      { Thursday: true },
+      { Friday: false },
+    ],
+  },
+  {
+    Jessa: [
+      { Monday: false },
+      { Tuesday: false },
+      { Wednesday: false },
+      { Thursday: false },
+      { Friday: true },
+    ],
+  },
 ];
 
 // YOUR CODE BELOW
+function attendanceCheck(day) {
+  let result = [];
+  for (let i = 0; i < classRoom.length; i++) {
+    for (let keys in classRoom[i]) {
+      for (let j = 0; j < classRoom[i][keys].length; j++) {
+        let test = classRoom[i][keys][j];
+        if (test[day] === true) {
+          result.push(keys);
+        }
+      }
+    }
+  }
+  return result;
+}
+console.log(attendanceCheck("Monday"));
+console.log(attendanceCheck("Wednesday"));
