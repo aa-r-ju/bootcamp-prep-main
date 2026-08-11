719d7b4b4c52ef5f5f8846f3f1dfad832bad74e0
aa-r-ju <aarju3447@gmail.com>
2026-08-11 17:12:13 +0545

    feat: say-my-name

--- workshops/08-object-methods/01-say-my-name/say-my-name.js
+++ workshops/08-object-methods/01-say-my-name/say-my-name.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+let me = {
+  name: "Kat",
+  getGreeting: function () {
+    return `Hi, my name is ${this.name}.`;
+  },
+};
+console.log(me.name);
+console.log(me.getGreeting());
