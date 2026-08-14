b19645e9748efc612d8f57e73b705cbe3fa31b9a
aa-r-ju <aarju3447@gmail.com>
2026-08-14 15:06:01 +0545

    feat: tic-tac-toe

--- workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
+++ workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
@@ -1,1 +1,26 @@
 // YOUR CODE BELOW
+let ticTacToe = {
+  board: [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ],
+  move: function (character, rowNum, colNum) {
+    if (this.board[rowNum][colNum] === null) {
+      this.board[rowNum][colNum] = character;
+    }
+    return this.board;
+  },
+  clear: function () {
+    return (this.board = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ]);
+  },
+};
+
+console.log(ticTacToe.move("y", 0, 0));
+console.log(ticTacToe.move("x", 1, 1));
+console.log(ticTacToe.move("z", 2, 2));
+console.log(ticTacToe.clear());
