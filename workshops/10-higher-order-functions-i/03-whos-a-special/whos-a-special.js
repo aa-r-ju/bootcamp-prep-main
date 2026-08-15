204cb25aa3978a42db9d0dee249f12c7d51742d3
aa-r-ju <aarju3447@gmail.com>
2026-08-15 22:28:55 +0545

    feat: whos-a-special

--- workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
+++ workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
@@ -1,1 +1,31 @@
 // YOUR CODE BELOW
+function whosASpecial(petArr) {
+  let str = "";
+
+  petArr.forEach(function (pet, i) {
+    if (i !== petArr.length - 1) {
+      str += `${pet.name} the ${pet.species} is very special! `;
+    } else {
+      str += `${pet.name} the ${pet.species} is very special!`;
+    }
+  });
+
+  return str;
+}
+
+let specialPets = [
+  {
+    name: "Sadie",
+    species: "cat",
+  },
+  {
+    name: "Layla",
+    species: "cat",
+  },
+  {
+    name: "Bogie",
+    species: "dog",
+  },
+];
+
+console.log(whosASpecial(specialPets));
