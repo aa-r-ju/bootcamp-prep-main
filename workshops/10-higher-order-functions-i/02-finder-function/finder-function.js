f802fb597dc22b6665dab8e336e93461babe2c26
aa-r-ju <aarju3447@gmail.com>
2026-08-15 22:05:29 +0545

    feat: finder-function

--- workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
+++ workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+function finderFunction(arr, callback) {
+  for (let i = 0; i < arr.length; i++) {
+    let result = callback(arr[i]);
+    if (result === true) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+let numbers = [1, 3, 5, 64, 7, 12];
+let odds = [9, 13, 15, 17];
+
+function isEven(num) {
+  return !(num % 2);
+}
+
+console.log(finderFunction(numbers, isEven)); // 3
+console.log(finderFunction(odds, isEven)); // -1
