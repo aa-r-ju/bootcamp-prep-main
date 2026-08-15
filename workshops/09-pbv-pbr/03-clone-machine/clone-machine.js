31eb35adcabf5cf3d05fb5913eb7361072595331
aa-r-ju <aarju3447@gmail.com>
2026-08-15 17:34:31 +0545

    feat: clone-machine

--- workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
+++ workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
@@ -1,1 +1,25 @@
 // YOUR CODE BELOW
+function cloneMachine(obj) {
+  let newObj = {};
+  for (let keys in obj) {
+    if (keys === "name") {
+      newObj[keys] = obj[keys] + "Clone";
+    } else if (keys === "offspring") {
+      newObj[keys] = [];
+      obj[keys].push(newObj.name);
+    } else {
+      newObj[keys] = obj[keys];
+    }
+  }
+  return newObj;
+}
+
+let dolly = {
+  name: "Dolly",
+  species: "sheep",
+  offspring: [],
+};
+
+let dollyClone = cloneMachine(dolly);
+console.log(dollyClone);
+console.log(dolly);
