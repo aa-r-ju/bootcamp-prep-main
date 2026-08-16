20374edd1fea9d2b68d00bdf406897f6799e05da
aa-r-ju <aarju3447@gmail.com>
2026-08-16 15:40:52 +0545

    feat: partial

--- workshops/11-higher-order-functions-ii/03-partial/partial.js
+++ workshops/11-higher-order-functions-ii/03-partial/partial.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+function partial(callback, arg) {
+  return function (num) {
+    return callback(arg, num);
+  };
+}
+
+function summer(a, b) {
+  return a + b;
+}
+
+let sumFive = partial(summer, 5);
+console.log(sumFive(10));
