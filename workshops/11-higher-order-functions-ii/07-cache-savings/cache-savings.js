55e7a90403418bf11921a25970bfd32cac9b6288
aa-r-ju <aarju3447@gmail.com>
2026-08-16 21:10:48 +0545

    feat: cache-savings

--- workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
+++ workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
@@ -1,1 +1,33 @@
 // YOUR CODE BELOW
+function cacheSavings(callback) {
+  let cache = {};
+  return function (arg) {
+    if (cache[arg] !== undefined) {
+      return cache[arg];
+    }
+
+    let result = callback(arg);
+    cache[arg] = result;
+
+    return result;
+  };
+}
+
+function adds10(num) {
+  let start = Date.now();
+  let end = Date.now();
+
+  // pause for 3 seconds, to make the function expensive!
+  while (end - start < 3000) {
+    end = Date.now();
+  }
+
+  return num + 10;
+}
+
+let cachedAdds10 = cacheSavings(adds10);
+
+console.log(cachedAdds10(20));
+console.log(cachedAdds10(0));
+
+console.log(cachedAdds10(20));
