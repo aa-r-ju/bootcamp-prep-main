028d8e1e3fe826cf60cde6cd91ef678c94b16581
aa-r-ju <aarju3447@gmail.com>
2026-08-16 15:30:56 +0545

    feat: biller-builder

--- workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
+++ workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+function billerBuilder(state) {
+  return function (price) {
+    let total = 0;
+    if (state === "NY") {
+      total = price * 1.03 * 1.04;
+    } else if (state === "NJ") {
+      total = price * 1.05 * 1.06625;
+    }
+    return total;
+  };
+}
+let newYorkBiller = billerBuilder("NY");
+console.log(newYorkBiller(100));
+let newJersBiller = billerBuilder("NJ");
+console.log(newJersBiller(100));
