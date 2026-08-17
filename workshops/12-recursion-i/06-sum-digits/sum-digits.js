5c7e942568a85942e4d6404c6cab484d6c91ffab
aa-r-ju <aarju3447@gmail.com>
2026-08-17 13:53:47 +0545

    feat: sum-digits

--- workshops/12-recursion-i/06-sum-digits/sum-digits.js
+++ workshops/12-recursion-i/06-sum-digits/sum-digits.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function sumDigits(numbers) {
+  if (numbers.length === 0) {
+    return 0;
+  }
+  let num = String(numbers);
+  return Number(num[num.length - 1]) + sumDigits(num.slice(0, -1));
+}
+
+console.log(sumDigits(1234));
