0b81ad4dfe55ef25e66c98eee77decfda3c5dff7
aa-r-ju <aarju3447@gmail.com>
2026-08-17 14:18:27 +0545

    feat:array-sum

--- workshops/13-recursion-ii/01-array-sum/README.md
+++ workshops/13-recursion-ii/01-array-sum/README.md
@@ -4,5 +4,5 @@ Write a function, arraySum, that accepts an array of numbers and returns the sum
 of all the numbers in the array (no matter how nested!).
 
 ```javascript
-arraySum([1, [2, 3, [4]]]) // => 10
+arraySum([1, [2, 3, [4]]]); // => 10
 ```
--- workshops/13-recursion-ii/01-array-sum/array-sum.js
+++ workshops/13-recursion-ii/01-array-sum/array-sum.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+function arraySum(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  if (typeof arr[0] === "number") {
+    return arr[0] + arraySum(arr.slice(1));
+  } else {
+    return arraySum(arr[0]) + arraySum(arr.slice(1));
+  }
+}
+console.log(arraySum([1, [2, 3, [4]]]));
