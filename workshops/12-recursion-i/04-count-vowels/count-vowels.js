6936a5e5dc06f03914b9579471482cde118a9902
aa-r-ju <aarju3447@gmail.com>
2026-08-17 13:22:48 +0545

    feat: count-vowels

--- workshops/12-recursion-i/04-count-vowels/count-vowels.js
+++ workshops/12-recursion-i/04-count-vowels/count-vowels.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function countVowels(str) {
+  if (str.length === 0) {
+    return 0;
+  }
+  let count = 0;
+  let vowels = "aeiou";
+  let currentVal = str[str.length - 1].toLowerCase();
+  if (vowels.includes(currentVal)) {
+    return 1 + countVowels(str.slice(0, -1));
+  } else {
+    return countVowels(str.slice(0, -1));
+  }
+}
+console.log(countVowels("Four score and seven years"));
