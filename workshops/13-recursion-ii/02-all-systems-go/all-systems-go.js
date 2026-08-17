9ee73325d72b75a1028cc6fd134cc3cf451956a9
aa-r-ju <aarju3447@gmail.com>
2026-08-17 14:42:15 +0545

    feat: all=system-go

--- workshops/13-recursion-ii/02-all-systems-go/all-systems-go.js
+++ workshops/13-recursion-ii/02-all-systems-go/all-systems-go.js
@@ -1,1 +1,56 @@
 // YOUR CODE BELOW
+function allSystemsGo(obj) {
+  if (typeof obj === "boolean") {
+    return obj;
+  }
+
+  let values = Object.values(obj);
+  console.log(values);
+
+  for (let i = 0; i < values.length; i++) {
+    if (!allSystemsGo(values[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+let systems = {
+  power: {
+    batteries: true,
+    solarCells: true,
+    generator: true,
+    fuelCells: true,
+  },
+  telecoms: {
+    antennas: {
+      highGain: true,
+      mediumGain: true,
+      lowGain: true,
+    },
+    transmitter: true,
+    receiver: true,
+  },
+  attitudeControl: {
+    stabilization: {
+      spin: true,
+      threeAxis: true,
+    },
+  },
+  propulsion: {
+    engines: {
+      engine1: true,
+      engine2: true,
+      engine3: false,
+    },
+    thrusters: true,
+    propellant: true,
+  },
+  environment: {
+    cooling: true,
+    heating: true,
+    lifeSupport: true,
+  },
+};
+
+allSystemsGo(systems);
