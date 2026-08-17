ad799753f8de4fd9d2cb30768b795fb75cd0b515
aa-r-ju <aarju3447@gmail.com>
2026-08-17 14:06:07 +0545

    feat: is-palindrome

--- workshops/12-recursion-i/07-is-palindrome/is-palindrome.js
+++ workshops/12-recursion-i/07-is-palindrome/is-palindrome.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function isPalindrome(word) {
+  if (word.length <= 1) {
+    return true;
+  }
+  if (word[0].toLowerCase() !== word[word.length - 1].toLowerCase()) {
+    return false;
+  }
+  return isPalindrome(word.slice(1, -1));
+}
+console.log(isPalindrome("Kayak"));
+console.log(isPalindrome("NEVERODDOREVEN"));
+console.log(isPalindrome("Tacocat"));
+console.log(isPalindrome("SELFLESS"));
