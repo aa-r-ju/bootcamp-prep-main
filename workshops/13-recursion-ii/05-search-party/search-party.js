b7d7b54500ee91830e5772c6bd13c2fecc070d96
aa-r-ju <aarju3447@gmail.com>
2026-08-20 20:27:09 +0545

    feat: search-party

--- workshops/13-recursion-ii/05-search-party/search-party.js
+++ workshops/13-recursion-ii/05-search-party/search-party.js
@@ -1,1 +1,36 @@
 // YOUR CODE BELOW
+function searchParty(name, world) {
+  for (let key in world) {
+    let value = world[key];
+
+    if (value === name) {
+      return [key];
+    }
+
+    if (Array.isArray(value)) {
+      for (let i = 0; i < value.length; i++) {
+        if (value[i] === name) {
+          return [key];
+        }
+      }
+    }
+
+    if (typeof value === "object" && value !== null && !Array.isArray(value)) {
+      let result = searchParty(name, value);
+
+      if (result !== null) {
+        return [key].concat(result);
+      }
+    }
+  }
+
+  return null;
+}
+
+let world = {
+  Fullstack: {
+    "11th floor": "Marge",
+    "25th floor": "Francis",
+  },
+  Subway: ["Jackie", "Grumio"],
+};
