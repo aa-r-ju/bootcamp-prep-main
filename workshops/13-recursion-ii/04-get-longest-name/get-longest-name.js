df921f62efbadd29b3842055d0204949631876cd
aa-r-ju <aarju3447@gmail.com>
2026-08-20 14:08:15 +0545

    feat: get-longest-name

--- workshops/13-recursion-ii/04-get-longest-name/get-longest-name.js
+++ workshops/13-recursion-ii/04-get-longest-name/get-longest-name.js
@@ -1,1 +1,26 @@
 // YOUR CODE BELOW
+function getLongestName(obj) {
+  if (obj === null) {
+    return "";
+  }
+
+  let longestName = "";
+
+  let names = Object.keys(obj);
+
+  for (let i = 0; i < names.length; i++) {
+    let name = names[i];
+
+    if (name.length > longestName.length) {
+      longestName = name;
+    }
+
+    let childResult = getLongestName(obj[name]);
+
+    if (childResult.length > longestName.length) {
+      longestName = childResult;
+    }
+  }
+
+  return longestName;
+}
